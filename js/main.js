8eef21a80f865333217a346da41074b5d3863ebb
Spence Nelson <spenjmn@me.com>
2016-05-17 11:33:31 -0400

    smooth scrolling

--- js/main.js
+++ js/main.js
@@ -36,3 +36,22 @@ $( document ).ready(function() {
 			setTimeout(function() { $(".copymessage").removeClass('copiedmessage'); }, 3000);
 	});
 });
+
+$( document ).ready(function() {
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+});
